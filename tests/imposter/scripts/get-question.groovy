a8514aa225b8682d3a32e46832e30cef3cb8910e
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2024-02-14 16:51:02 +0000

    test: Fix stateful request in Imposter stub

--- tests/imposter/scripts/get-question.groovy
+++ tests/imposter/scripts/get-question.groovy
@@ -4,16 +4,15 @@ def questionStore = stores.open("questions");
 def questions = questionStore.load("questions");
 
 def answers = stores.open("answers").loadAll();
-def unansweredQuestions = questions.findAll {question -> !answers.containsKey(question.questionKey)};
 
-if (unansweredQuestions.size() === 0) {
+if (answers.size() >= 1) {
     respond {
         withStatusCode(204)
     }
     return
 }
 
-def currentQuestion = unansweredQuestions[0];
+def currentQuestion = questions[0];
 
 respond {
     withStatusCode(200)
